4c784d9b56610e95e8ccf35e17cc4fe078259991
Johan K. Jensen <jj@johanjensen.dk>
2016-01-06 11:03:24 +0100

    Use my forks of Swift Libraries

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "tldr",
     dependencies: [
         // Custom version of PrettyColors to support Swift Package Manager
-        .Package(url: "../PrettyColors", majorVersion: 3),
+        .Package(url: "https://github.com/johankj/PrettyColors", majorVersion: 3),
         // Custom version of CommandLine to support positional arguments
-        .Package(url: "../CommandLine", majorVersion: 2)
+        .Package(url: "https://github.com/johankj/CommandLine", majorVersion: 2)
     ]
 )
